a29112072fe923f7538d9872555f72d870b1570e
Geocarlos <galves@inair.studio>
2022-04-21 16:12:39 -0300

    Add ReactPrime

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,6 +1,8 @@
 import {initializeBlock} from '@airtable/blocks/ui';
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 import './index.scss';
 
 const App = () => <div className="container">Hello from InAir Studio!</div>;
